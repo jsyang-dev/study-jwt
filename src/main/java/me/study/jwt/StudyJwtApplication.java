1e0b262cb5c965f8e0691ebdf1bf7a1c57a47c77
Jeongsu Yang <mycat83@gmail.com>
2021-10-16 23:17:27 +0900

    Initial commit

---
+++ src/main/java/me/study/jwt/StudyJwtApplication.java
@@ -0,0 +1,13 @@
+package me.study.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJwtApplication.class, args);
+    }
+
+}
