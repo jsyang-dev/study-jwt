8cfb3533960a6938df5629ef836fd78836cacd07
Jeongsu Yang <mycat83@gmail.com>
2021-10-20 22:22:01 +0900

    feat: DTO, Repository, 로그인

---
+++ src/main/java/me/study/jwt/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package me.study.jwt.repository;
+
+import me.study.jwt.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
